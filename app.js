26922b9b5d538ddc43b9b921d4f66d3d8aa20458
razahamza0308 <razahamza0308@gmail.com>
2021-05-26 22:08:44 +0500

    ccc

---
+++ app.js
@@ -0,0 +1,112 @@
+
+//****************Question 1 *********************/
+
+
+
+// var userNum=prompt("Enter any positive numbeer for round off");
+// var round=Math.round(userNum);
+// var floor=Math.floor(userNum);
+// var ceil=Math.ceil(userNum);
+// var mynum=false;
+// for(var i=0;i<usernum.length;i++){
+//  if(userNum[i]==='-'){
+//      myNum=true;
+//     document.write("Please enter only positive numbers");
+// break;
+//  }
+// }
+// if(mynum===false){
+//     document.write("Number "+userNum+"</br>");
+//     document.write("Round-off  "+round+"</br>");
+//     document.write("Floor "+floor+"</br>");
+//     document.write("Ceil "+ceil+"</br>");
+
+// }
+
+//****************Question  2 *********************/
+
+
+// var usernum=prompt("Enter any negative numbeer for round off");
+// var round=Math.round(usernum);
+// var floor=Math.floor(usernum);
+// var ceil=Math.ceil(usernum);
+// var mynum=false;
+// for(var i=0;i<usernum.length;i++){
+//  if(usernum[i]!=='-'){
+//      mynum=true;
+//     document.write("Please enter only negative numbers");
+//  }
+//  break;
+// }
+// if(mynum===false){
+//     document.write("Number "+usernum+"</br>");
+//     document.write("Round-off  "+round+"</br>");
+//     document.write("Floor "+floor+"</br>");
+//     document.write("Ceil "+ceil+"</br>");
+
+// }
+
+
+
+//****************Question 3 *********************/
+
+
+// var userval=prompt("Enter any number");
+// var absolute=Math.abs(userval);
+// document.write("Absolute value of "+userval+" is "+absolute);
+
+
+
+
+//****************Question 4 *********************/
+
+
+
+// var dice=Math.random()*6;
+// var res=Math.ceil(dice);
+// document.write("Dice no "+res);
+
+
+
+//****************Question 5 *********************/
+
+
+
+// var coin = Math.random()*2;
+// var res=Math.ceil(coin);
+// if(res===1){
+//     document.write("Random Coin value is : Head")
+// }
+// else{
+//     document.write("Random Coin value is :Tail");
+// }
+
+
+
+//****************Question 6 *********************/
+
+
+
+// var no= Math.random()*100;
+// var res=Math.ceil(no);
+// document.write(res);
+
+
+
+//****************Question 8 *********************/
+
+
+// var no= Math.random()*10;
+// var res=Math.ceil(no);
+// var userval=prompt("Enter your no");
+// if(res===userval){
+//     document.write("Your no is "+userval+"</br>");
+//     document.write("Secret no is "+res+"</br>");
+//     document.write("Congratulation you have won this game");
+// }
+// else{
+//     document.write("Your no is "+userval+"</br>");
+//     document.write("Secret no is "+res+"</br>");
+//     document.write("sorry you have lost this game");
+// }
+
